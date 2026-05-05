e467274fe24c63314ef67de1356d87f3f9ceafd5
Avani Garg <142567720+avnigarg12@users.noreply.github.com>
2026-05-05 12:13:46 +0530

    changes

--- NODE JS LAB/app.js
+++ NODE JS LAB/app.js
@@ -6,13 +6,6 @@ const Event = require('./models/event');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose
-  .connect('mongodb://127.0.0.1:27017/eventLab', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
